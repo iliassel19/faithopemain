3c892332abead62a35228f4014354ecc6476024e
iliassel19 <89216825+iliassel19@users.noreply.github.com>
2022-03-31 20:22:27 +0000

    Update js.js

--- js.js
+++ js.js
@@ -17,3 +17,29 @@ const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
 
 
+
+const allLinks = document.querySelectorAll("a:link");
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
+
+
