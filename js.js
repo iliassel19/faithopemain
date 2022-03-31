f2fe28c5ee4cabb2b1c5783f3ac6be8279e61768
iliassel19 <89216825+iliassel19@users.noreply.github.com>
2022-03-31 20:33:21 +0000

    Update js.js

--- js.js
+++ js.js
@@ -20,18 +20,3 @@ yearEl.textContent = currentYear;
 
 
 
-const allLinks = document.querySelectorAll("a:link");
-
-allLinks.forEach(function (link) {
-  link.addEventListener("click", function (e) {
-    e.preventDefault();
-    const href = link.getAttribute("href");
-
-
-    // Scroll to other links
-    if (href !== "#" && href.startsWith("#")) {
-      const sectionEl = document.querySelector(href);
-      sectionEl.scrollIntoView({ behavior: "smooth" });
-    }
-  });
-});
